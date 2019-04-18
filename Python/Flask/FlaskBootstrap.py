c2eab69f0487df3c63fcc4d9ee37305bdf387bc1
xieyue1 <johndoe@example.com>
2019-04-18 18:34:54 +0800

    自定义错误样式

--- Python/Flask/FlaskBootstrap.py
+++ Python/Flask/FlaskBootstrap.py
@@ -11,6 +11,17 @@ def index():
 @app.route('/buser/<name>')
 def buser(name):
     return render_template('bootstrap_user.html', name = name)
+
+# 客户端请求位置页面或路由
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# 处理异常显示
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     bootstrap = Bootstrap(app)
     app.run()
