546c6709a9cdfe24da4ad7d11a63af387b0eacc6
xieyue1 <johndoe@example.com>
2019-05-07 20:26:57 +0800

    每K个节点为一组翻转链表

--- Interview/链表/每k个节点一组翻转链表.cpp
+++ Interview/链表/每k个节点一组翻转链表.cpp
@@ -1,7 +1,3 @@
-
-/*
-	
-*/
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
@@ -13,18 +9,22 @@ public:
         ListNode* cur = head;
         while((container.size() < k) && cur){
             container.push(cur); 
-            ListNode* next = cur->next;
+            /*
+				注意：此处需要将下一个节点进行备份，否则会进入死循环
+				假设k=2,链表为1 -> 2 -> 3 -> 4 -> 5
+				第一次读取到k个元素之后，此时cur为2所在位置
+				翻转得到2 -> 1 -> 3 -> 4 -> 5
+				如果cur->next没有备份，此时2所在位置的next为1所对应的指针，又循环回到1 -> 2 -> 3 -> 4 -> 5的读取
+			*/
+			ListNode* next = cur->next;
             if(container.size() == k){
                 if(!pre){
                     pre = container.top();
                     container.pop();
-                    pre->next = NULL;
-                }
+                }                
                 while(!container.empty()){
                     if(!res) res = pre;
-                    ListNode* tmp = container.top();
-                    tmp->next = NULL;
-                    pre->next = tmp;
+                    pre->next = container.top();
                     container.pop();
                     pre = pre->next;
                 }
