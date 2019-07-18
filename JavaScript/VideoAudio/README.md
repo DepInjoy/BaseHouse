a943bb41459ba014bb4b3cddd0a65b5aa4b74e5e
xieyue1 <xieyue1@jd.com>
2019-07-18 09:29:38 +0800

    webRTC获取摄像头音视频并播放

--- JavaScript/VideoAudio/README.md
+++ JavaScript/VideoAudio/README.md
@@ -4,5 +4,10 @@
     功能描述：获取本地摄像头的音频和视频，并在html中进行播放
 
 第三方库
-- [mediastreamconstraints媒体轨限制](https://w3c.github.io/mediacapture-main/getusermedia.html#mediastreamconstraints)
+- 音视频采集
+    - navigator.mediaDevices.getUserMedia(constraints);
+        - constraints：类型为 MediaStreamConstraints，指定包含哪些视媒体轨，并为这些媒体轨设置一些限制。
+        - 返回：成功则返回MediaStream对象；失败则会返回PermissionDeniedError或NotFoundError等错误对象。
+
+- [MediaStreamConstraints 媒体轨限制](https://w3c.github.io/mediacapture-main/getusermedia.html#mediastreamconstraints)
     
--- JavaScript/VideoAudio/ch1/GetLocalVideoAudio.js
+++ JavaScript/VideoAudio/ch1/GetLocalVideoAudio.js
@@ -1,18 +1,25 @@
 'use strict';
 
 //媒体轨限制
-const mediaStreamContrains = {
+const mediaStreamConstrains = {
     video: {
-        frameRate: {min: 20},
-        width: {min: 640, ideal: 1280},
-        height: {min: 360, ideal: 720},
-        aspectRatio: 16/9
-    },
-    audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        autoGainControl: true,
-        muted:           true
+        frameRate: {
+            min:                20,         //帧率最小值
+        },
+        width: {
+            min:                640,        //宽度最小值
+            ideal:              1280        //理想宽度
+        },
+        height: {
+            min:                360,        //高度最小值
+            ideal:              720         //理想高度
+        },
+        aspectRatio:            16/9        //宽高比
+    }, audio: {
+        echoCancellation:       true,       //是否开启回音消除
+        noiseSuppression:       true,       //是否开启降噪
+        autoGainControl:        true,       //是否开启自动增益
+        muted:                  true,
     }
 };
 
@@ -35,8 +42,14 @@ function handleLocalMediaStreamError(error){
     console.log('navigator.getUserMedia error: ', error);
 }
 
-navigator.mediaDevices.getUserMedia(mediaStreamContrains).then(
+/**
+ * 功能描述：                      视音频采集
+ * mediaStreamConstrains：       媒体轨限制
+ */
+navigator.mediaDevices.getUserMedia(mediaStreamConstrains).then(
+    //陈宫获取视频处理
     gotLocalMediaStream
 ).catch(
+    //错误处理
     handleLocalMediaStreamError
 );
